8481b3e3a06af1492b78b05c7d8ef5107b6bce9f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-07 21:13:22 +0200

    feat: Check if entity is a hostile mob

--- src/main/kotlin/listeners/EntitySpawn.kt
+++ src/main/kotlin/listeners/EntitySpawn.kt
@@ -1,5 +1,7 @@
 package com.github.DarkVanityOfLight.AntiMobs.listeners
 
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
@@ -8,6 +10,8 @@ class EntitySpawn : Listener{
 
     @EventHandler
     fun onEntitySpawn(event: EntitySpawnEvent){
-
+        if (event.entity is LivingEntity && event.entity !is  Player){
+            //TODO Check if entity is in faction area
+        }
     }
 }
