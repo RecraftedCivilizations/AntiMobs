b2897f1db9a195948be7ac25466d7d603079ebaf
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-07 21:07:37 +0200

    feat: register event

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 package com.github.DarkVanityOfLight.AntiMobs
 
+import com.github.DarkVanityOfLight.AntiMobs.listeners.EntitySpawn
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin(){
 
     override fun onEnable() {
 
+        Bukkit.getPluginManager().registerEvents(EntitySpawn(), this)
     }
 }
