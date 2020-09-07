8dd3564db4204f86905bf4e101c8b512b2bd1645
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-07 21:08:54 +0200

    fix: Notify event handler

--- src/main/kotlin/listeners/EntitySpawn.kt
+++ src/main/kotlin/listeners/EntitySpawn.kt
@@ -1,10 +1,12 @@
 package com.github.DarkVanityOfLight.AntiMobs.listeners
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
 
 class EntitySpawn : Listener{
 
+    @EventHandler
     fun onEntitySpawn(event: EntitySpawnEvent){
 
     }
