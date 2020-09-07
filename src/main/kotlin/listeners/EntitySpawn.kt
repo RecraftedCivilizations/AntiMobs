8f8895879e2307e127a39897b180500b56df18a5
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-07 21:07:04 +0200

    feat: Basiv event implementation

---
+++ src/main/kotlin/listeners/EntitySpawn.kt
@@ -0,0 +1,11 @@
+package com.github.DarkVanityOfLight.AntiMobs.listeners
+
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntitySpawnEvent
+
+class EntitySpawn : Listener{
+
+    fun onEntitySpawn(event: EntitySpawnEvent){
+
+    }
+}
