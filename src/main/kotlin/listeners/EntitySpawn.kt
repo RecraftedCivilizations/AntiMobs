5ccbd65c260d362032d82e5b40af228acbdfdba0
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-09 22:07:25 +0200

    feat: cancel monster spawn event, when they are not allowed to

--- src/main/kotlin/listeners/EntitySpawn.kt
+++ src/main/kotlin/listeners/EntitySpawn.kt
@@ -1,5 +1,6 @@
 package com.github.DarkVanityOfLight.AntiMobs.listeners
 
+import com.github.DarkVanityOfLight.AntiMobs.parsers.ConfigParser
 import com.massivecraft.factions.Board
 import com.massivecraft.factions.FLocation
 import com.massivecraft.factions.Faction
@@ -11,13 +12,16 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
 
-class EntitySpawn : Listener{
+class EntitySpawn(val configParser: ConfigParser) : Listener{
 
     @EventHandler
     fun onEntitySpawn(event: EntitySpawnEvent){
         if (event.entity is LivingEntity && event.entity !is  Player){
             val board : Board = Board.getInstance()
             val faction : Faction = board.getFactionAt(FLocation(event.entity.location))
+            if (faction.tag !in configParser.enabledSpawnFactions || (!configParser.spawnInWilderness && faction.isWilderness)){
+                event.isCancelled = true
+            }
 
         }
     }
