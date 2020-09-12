ae54c1eeaee6c05a0338b9681cc03dc54b3c440a
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-12 14:36:19 +0200

    fix: hopefully last fix for the spawn condition

--- src/main/kotlin/listeners/EntitySpawn.kt
+++ src/main/kotlin/listeners/EntitySpawn.kt
@@ -5,31 +5,27 @@ import com.github.DarkVanityOfLight.AntiMobs.parsers.ConfigParser
 import com.massivecraft.factions.Board
 import com.massivecraft.factions.FLocation
 import com.massivecraft.factions.Faction
-import com.massivecraft.factions.FactionsPlugin
-import org.bukkit.Bukkit
-import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Monster
-import org.bukkit.entity.Player
 import org.bukkit.entity.Slime
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
 
-class EntitySpawn(val configParser: ConfigParser) : Listener{
+class EntitySpawn(val configParser: ConfigParser) : Listener {
 
     @EventHandler(ignoreCancelled = true)
-    fun onEntitySpawn(event: EntitySpawnEvent){
-        if (event.entity is Monster || event.entity is Slime){
-            val board : Board = Board.getInstance()
-            val faction : Faction = board.getFactionAt(FLocation(event.entity.location))
-            if (faction.tag !in configParser.enabledSpawnFactions && (!configParser.spawnInWilderness && faction.isWilderness)){
+    fun onEntitySpawn(event: EntitySpawnEvent) {
+        if (event.entity is Monster || event.entity is Slime) {
+            val board: Board = Board.getInstance()
+            val faction: Faction = board.getFactionAt(FLocation(event.entity.location))
+            if ((faction.tag !in configParser.enabledSpawnFactions && !faction.isWilderness) ||
+                (faction.isWilderness && !configParser.spawnInWilderness)) {
                 event.isCancelled = true
 
-                if (configParser.debug){
+                if (configParser.debug) {
                     Main.debugLog("Cancelled mob spawning")
                 }
             }
-
         }
     }
 }
